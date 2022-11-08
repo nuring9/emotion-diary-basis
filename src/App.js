cd331d174ba0396893cbcd90f2af3271bbc5485c
nuri <for9595@gmail.com>
2022-11-08 16:21:36 +0900

    React에서 사용자 입력 처리하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import DiaryEditor from './DiaryEditor';
 
 function App() {
+
   return (
-    <div className="App">
-      
+    <div className="App">   
+      <DiaryEditor/>
     </div>
   );
 }

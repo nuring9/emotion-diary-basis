b121dcfefaf928995d7ce7c2e081af59253e29f3
nuri <for9595@gmail.com>
2022-11-08 19:38:07 +0900

    배열1_리스트렌더링(조회)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "가나다",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "라마바",
+    content: "하이 2",
+    emotion: 3,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "사아자",
+    content: "하이 3",
+    emotion: 4,
+    created_date: new Date().getTime()
+  }
+]
 
 function App() {
-
   return (
     <div className="App">   
-      <DiaryEditor/>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
-}
+};
 
 export default App;

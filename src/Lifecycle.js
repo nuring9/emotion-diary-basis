157c0cdc1f04b914b89966bf5ebde399068f3ef5
nuri <for9595@gmail.com>
2022-11-12 17:12:02 +0900

    ReactLifecycle_ useEffect_2

--- src/Lifecycle.js
+++ src/Lifecycle.js
@@ -1,38 +1,25 @@
 import React, { useState, useEffect } from "react";
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
+const UnmountTest = () => {
   useEffect(() => {
     console.log("Mount!");
-  }, []); // 처음 mount인 상태일때
+    return () => {
+      //return을 사용하여 Unmount 시점에서 실행되게 됨.
+      console.log("Unmount!");
+    };
+  }, []);
 
-  useEffect(() => {
-    console.log("Update!");
-  }); // 페이지가 리렌더링 될때 (항상 업데이트 될때)
+  return <div>Unmount Testing component</div>;
+};
 
-  useEffect(() => {
-    console.log(`count is update:  ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다 따라서 1로 초기화 합니다");
-      setCount(1);
-    }
-  }, [count]); // count가 update 될때
-
-  useEffect(() => {
-    console.log(`text is update:  ${text}`);
-  }, [text]); // text가 update 될때
+const Lifecycle = () => {
+  const [isVisible, setIsvisible] = useState(false);
+  const toggle = () => setIsvisible(!isVisible);
 
   return (
-    <div style={{ padding: 10 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>+</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+    <div stayle={{ padding: 20 }}>
+      <button onClick={toggle}>ON/OFF</button>
+      {isVisible && <UnmountTest />}
     </div>
   );
 };

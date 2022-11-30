dba1a2c5ce32bad71dca89cf7a3f3b1d1f536137
nuri <for9595@gmail.com>
2022-11-30 19:08:21 +0900

    최적화2_ React.memo_2

--- src/OptimizeTest.js
+++ src/OptimizeTest.js
@@ -1,34 +1,52 @@
 import React, { useState, useEffect } from "react";
 
-const Textview = React.memo(({ text }) => {
+const CounterA = React.memo(({ count }) => {
   useEffect(() => {
-    console.log(`Update :: Text : ${text}`);
-  });
-  return <div>{text}</div>;
-});
-
-const Countview = React.memo(({ count }) => {
-  useEffect(() => {
-    console.log(`Update :: Count : ${count}`);
+    console.log(`CounterA Update - count: ${count}`);
   });
   return <div>{count}</div>;
 });
 
+const CounterB = ({ obj }) => {
+  useEffect(() => {
+    console.log(`ConterB Update - count: ${obj.count}`);
+  });
+  return <div>{obj.count}</div>;
+};
+
+const areEqual = (prevprops, nextprops) => {
+  if (prevprops.obj.count === nextprops.obj.count) {
+    return true;
+  }
+  return false;
+};
+
+/*
+const areEqual = (prevprops, nextprops) => {
+ return prevprops.obj.count === nextprops.obj.count;
+};
+위의 const areEqual을 이렇게 축약하여 사용할 수도 있다. 
+*/
+
+const MemoizedCounterB = React.memo(CounterB, areEqual);
+
 const OptimizeTest = () => {
   const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
+  const [obj, setObj] = useState({
+    count: 1,
+  });
 
   return (
     <div style={{ padding: 50 }}>
       <div>
-        <h2>count</h2>
-        <Countview count={count} />
-        <button onClick={() => setCount(count + 1)}>+</button>
+        <h2>Counter A</h2>
+        <CounterA count={count} />
+        <button onClick={() => setCount(count)}>A button</button>
       </div>
       <div>
-        <h2>text</h2>
-        <Textview text={text} />
-        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <h2>Counter B</h2>
+        <MemoizedCounterB obj={obj} />
+        <button onClick={() => setObj({ count: obj.count })}>B button</button>
       </div>
     </div>
   );
